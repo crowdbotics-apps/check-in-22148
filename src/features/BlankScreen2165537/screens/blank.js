18d34d14a1fb3154e0e6fbddf16f567032fe9833
Crowdbotics <team@crowdbotics.com>
2020-10-31 01:52:31 +0000

    Update components

--- src/features/BlankScreen2165537/screens/blank.js
+++ src/features/BlankScreen2165537/screens/blank.js
@@ -23,7 +23,17 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { CheckBox_2: true, CheckBox_3: true, CheckBox_4: true }
+  state = {
+    CheckBox_2: true,
+    CheckBox_3: true,
+    CheckBox_4: true,
+    CheckBox_5: true,
+    CheckBox_6: true,
+    CheckBox_7: true,
+    CheckBox_8: true,
+    CheckBox_9: true,
+    CheckBox_10: true
+  }
 
   render = () => (
     <View>
@@ -42,6 +52,36 @@ export default class Blank extends React.Component {
         checked={this.state.CheckBox_4}
         onPress={nextChecked => this.setState({ CheckBox_4: nextChecked })}
       />
+      <CheckBox
+        title="Shame"
+        checked={this.state.CheckBox_5}
+        onPress={nextChecked => this.setState({ CheckBox_5: nextChecked })}
+      />
+      <CheckBox
+        title="Joy"
+        checked={this.state.CheckBox_6}
+        onPress={nextChecked => this.setState({ CheckBox_6: nextChecked })}
+      />
+      <CheckBox
+        title="Helplessness"
+        checked={this.state.CheckBox_7}
+        onPress={nextChecked => this.setState({ CheckBox_7: nextChecked })}
+      />
+      <CheckBox
+        title="Irritability ( do you get annoyed or mad really quickly "
+        checked={this.state.CheckBox_8}
+        onPress={nextChecked => this.setState({ CheckBox_8: nextChecked })}
+      />
+      <CheckBox
+        title="Loneliness"
+        checked={this.state.CheckBox_9}
+        onPress={nextChecked => this.setState({ CheckBox_9: nextChecked })}
+      />
+      <CheckBox
+        title="Mood Swings"
+        checked={this.state.CheckBox_10}
+        onPress={nextChecked => this.setState({ CheckBox_10: nextChecked })}
+      />
     </View>
   )
 }
@@ -51,8 +91,19 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   CheckBox_2: {},
   CheckBox_3: {},
-  CheckBox_4: {}
+  CheckBox_4: {},
+  View_1: {},
+  CheckBox_2: {},
+  CheckBox_3: {},
+  CheckBox_4: {},
+  CheckBox_5: {},
+  CheckBox_6: {},
+  CheckBox_7: {},
+  CheckBox_8: {},
+  CheckBox_9: {},
+  CheckBox_10: {}
 })
