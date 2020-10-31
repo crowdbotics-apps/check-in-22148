7037b6c7c87681130013d857ded585569e145fc1
Crowdbotics <team@crowdbotics.com>
2020-10-31 01:32:11 +0000

    Update models

--- backend/check_in_22148/wsgi.py
+++ backend/check_in_22148/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'check_in_22148.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "check_in_22148.settings")
 
 application = get_wsgi_application()
